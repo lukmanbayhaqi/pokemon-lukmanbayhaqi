55f9e0e595d66a17479644210307d09f1dcbf15a
lukman baihaqi <lukmanbaihaqi78@gmail.com>
2021-07-27 21:29:25 +0700

    create navbar and install some packages

--- src/main.js
+++ src/main.js
@@ -1,13 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import "bootstrap/dist/css/bootstrap.css";
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+import "./styles/style.css";
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
