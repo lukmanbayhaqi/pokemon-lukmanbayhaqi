13d421863eee07428b82961e7f11480e95df74b0
lukman baihaqi <lukmanbaihaqi78@gmail.com>
2021-07-28 20:32:04 +0700

    add history modal and add some tutorial on readme.md

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <template>
   <div id="app">
     <navbar />
+
     <b-container class="p-5" id="app-container" fluid>
       <router-view />
     </b-container>
+
+    <history />
   </div>
 </template>
 
 <script>
 import Navbar from "@/components/navbar";
+import History from "@/components/history";
 
 export default {
   components: {
     Navbar,
+    History,
   },
   mounted() {
     this.loadData();
